1dcc4d3c2ce77276b689e86eb1e94009e25e9975
junmo-l <wlq1502@gmail.com>
2022-07-24 21:43:16 +0900

    all commit

--- portfolio_js01/app.js
+++ portfolio_js01/app.js
@@ -15,12 +15,68 @@
 // let something;
 // console.log(something);
 
+// array
 // const daysOfWeek = ["mon", "tue", "wde", "thu", "fri", "sat", "sun"];
 // console.log(daysOfWeek[6]);
 
 // daysOfWeek.push("sunday");
 // console.log(daysOfWeek[7]);
 
-const toBuy = ["potato", "tomato", "pizza"];
-toBuy.push("kimbab");
-console.log(toBuy);
+// const toBuy = ["potato", "tomato", "pizza"];
+// toBuy.push("kimbab");
+// console.log(toBuy);
+
+// object
+// const player = {
+//   name: "junmo",
+//   points: 10,
+//   fat: true,
+// };
+// console.log(player);
+// player.lastname = "potato";
+// console.log(player);
+// player.points = player.points + 15;
+// console.log(player.points);
+// console.log(player);
+
+// funtion
+// function sayHello(){
+//     console.log("Hello my name is C");
+// }
+// console.log("hello");
+// sayHello("junmo");
+// sayHello("mojun");
+// sayHello("leejun");
+
+// function sayHello(nameOfPerson, age) {
+//   console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
+// }
+// // 값넣기
+// sayHello("junmo", 10);
+// sayHello("mojun", 20);
+// sayHello("leejun", 30);
+
+// function plus(a, b) {
+//   console.log(a + b);
+// }
+// plus(8, 60);
+
+// function divide(a, b) {
+//   console.log(a / b);
+// }
+// divide(100, 2);
+
+// const player = {
+//   name: "junmo",
+//   sayHello: function (otherPersonsName) {
+//     console.log("hello! " + otherPersonsName + " nice to meet you!");
+//   },
+// };
+
+// console.log(player.name);
+// player.sayHello("msoso");
+
+function plus(a, b) {
+  console.log(a + b);
+}
+plus(5, 6);
