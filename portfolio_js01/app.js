1e7e5cf12daab7f74e3969b2a52195876b404f5a
junmo-l <wlq1502@gmail.com>
2022-07-24 20:24:46 +0900

    all commit

---
+++ portfolio_js01/app.js
@@ -0,0 +1,26 @@
+// const a = 5;
+// const b = 2;
+
+// let myName = "junmo";
+// console.log(a + b);
+// console.log("hello " + myName);
+
+// myName = "junmoLee";
+
+// console.log("your new name is " + myName);
+
+// const amIFat = null;
+// console.log(amIFat);
+
+// let something;
+// console.log(something);
+
+// const daysOfWeek = ["mon", "tue", "wde", "thu", "fri", "sat", "sun"];
+// console.log(daysOfWeek[6]);
+
+// daysOfWeek.push("sunday");
+// console.log(daysOfWeek[7]);
+
+const toBuy = ["potato", "tomato", "pizza"];
+toBuy.push("kimbab");
+console.log(toBuy);
