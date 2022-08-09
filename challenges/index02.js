6f47eb40393cbee1cddfdcaf9fcb68f969950966
junmo-l <wlq1502@gmail.com>
2022-08-09 19:16:53 +0900

    ac

--- challenges/index02.js
+++ challenges/index02.js
@@ -2,7 +2,7 @@ const selectNum1 = document.querySelector("#selectNum1");
 const num1 = document.querySelector("#num1");
 const selectNum2 = document.querySelector("#selectNum2");
 const num2 = document.querySelector("#num2");
-const btn = document.querySelector("#btn");
+// const btn = document.querySelector("#btn");
 const value1 = document.querySelector("#value1");
 const value2 = document.querySelector("#value2");
 
@@ -18,9 +18,10 @@ function onNum2Submit(event) {
   if (check) {
     randomNumber = Math.ceil(Math.random() * parseInt(num1.value));
   } else {
+    num2.value = 0;
     alert(`${num1.value}보다 작은 값을 넣어주세요.`);
   }
-  console.log(parseInt(num2.value) > randomNumber);
+
   if (parseInt(num2.value) > randomNumber) {
     value1.innerText = `You chose : ${num2.value}, the machine chose : ${randomNumber}.`;
     value2.innerText = "You Winner!";
